20534fee9cf92d782ce41ef0151e027829861010
Daniela Fernandez <daniela.fernandez7@gmail.com>
2019-06-13 02:30:39 -0400

    new

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,25 @@
 class TodosController < ApplicationController
+  before_action :find_todo, only: [:show, :edit, :update, :destroy, :completed]
   def index
     @todos = Todo.all
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+  @todo = Todo.new(todo_params)
+  @todo.save
+  redirect_to todos_path
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:description, :completed)
+  end
+
+  def find_todo
+  @todo = Todo.find(params[:id])
   end
 end
